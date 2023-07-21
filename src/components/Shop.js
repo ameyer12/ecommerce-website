0090e3e9f409cc78d2c261fb4f7b05cb0805537b
Austin <austinmeyer1106@gmail.com>
2023-07-21 14:50:32 -0500

    added redirects file

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -10,6 +10,8 @@ function Shop() {
   const [productId, setProductId] = useState(1);
   const [quantity, setQuantity] = useState(0); 
 
+  const token = window.localStorage.token;
+
   const fetchProducts =  async () => {
     try {
         const response = await axios.get('https://ecommerce-website-60o8.onrender.com/api/products')
@@ -45,34 +47,58 @@ function Shop() {
     fetchProducts()
   }, [])
 
-  return (
-    <div className="shop">
-        <h1 className='shop-page-h1'>Shop All</h1>
-        <div className='products-container'> {
-            products.map((currentItem, index) => { 
-                return <li className="card" key={index}>
-                            <img className="product-image" src={currentItem[2]} alt="product"/>
-                            <p className='product-title'>
-                                {currentItem[1]}
-                                <br></br>
-                                ${currentItem[4]}
-                                <br></br>
-                                <button
-                                  className="btn add-product"
-                                  onClick={() => {
-                                    setProductId(currentItem[0]);
-                                    setQuantity((prevQuantity) => prevQuantity + 1)
-                                    addProductToCart()
+  if(token){
+    return (
+      <div className="shop">
+          <h1 className='shop-page-h1'>Shop All</h1>
+          <div className='products-container'> {
+              products.map((currentItem, index) => { 
+                  return <li className="card" key={index}>
+                              <img className="product-image" src={currentItem[2]} alt="product"/>
+                              <p className='product-title'>
+                                  {currentItem[1]}
+                                  <br></br>
+                                  ${currentItem[4]}
+                                  <br></br>
+                                  <button
+                                    className="btn add-product"
+                                    onClick={() => {
+                                      setProductId(currentItem[0]);
+                                      setQuantity((prevQuantity) => prevQuantity + 1)
+                                      addProductToCart()
+                                      }
                                     }
-                                  }
-                                >Add Item to Cart</button>
-                            </p>
-                        </li>
-            })}
-        </div>
-        <div className='spacing-div'></div>
-    </div>
-  );
+                                  >Add Item to Cart</button>
+                              </p>
+                          </li>
+              })}
+          </div>
+          <div className='spacing-div'></div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="shop">
+          <h1 className='shop-page-h1'>Shop All</h1>
+          <div className='products-container'> {
+              products.map((currentItem, index) => { 
+                  return <li className="card" key={index}>
+                              <img className="product-image" src={currentItem[2]} alt="product"/>
+                              <p className='product-title'>
+                                  {currentItem[1]}
+                                  <br></br>
+                                  ${currentItem[4]}
+                                  <br></br>
+                                  <br></br>
+                                  Sign in or register to add items to a cart
+                              </p>
+                          </li>
+              })}
+          </div>
+          <div className='spacing-div'></div>
+      </div>
+    );
+  }
 }
 
 export default Shop;
